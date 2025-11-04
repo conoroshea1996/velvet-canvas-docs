d0524d58ca26dfe9e7190b3be9f0d6c9597bb506
con o shea <conoshea12@gmail.com>
2025-11-04 19:07:04 +0000

    runtime update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import { run } from 'svelte/legacy';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs20'
+		})
 	}
 };
 
