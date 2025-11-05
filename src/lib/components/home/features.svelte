d217577487e621102c75d72570db50845acfede9
con o shea <conoshea12@gmail.com>
2025-11-05 17:35:25 +0000

    - quick clean up

--- src/lib/components/home/features.svelte
+++ src/lib/components/home/features.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { features } from '$lib/config';
-	import { CanvasBoard, CanvasContext } from 'velvet-canvas';
-	import Toolbox from '../Toolbox.svelte';
+	import { features } from "$lib/config";
+	import { CanvasBoard, CanvasContext } from "velvet-canvas";
+	import Toolbox from "../Toolbox.svelte";
 </script>
 
 <div class="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
@@ -12,10 +12,14 @@
 				<div
 					class="mx-auto flex size-12 items-center justify-center rounded-full border border-gray-200 bg-gray-50 dark:border-neutral-700 dark:bg-neutral-800"
 				>
-					<feature.icon class="text-primary size-5 shrink-0 dark:text-neutral-400" />
+					<feature.icon
+						class="text-primary size-5 shrink-0 dark:text-neutral-400"
+					/>
 				</div>
 				<div class="mt-3">
-					<h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+					<h3
+						class="text-lg font-semibold text-gray-800 dark:text-white"
+					>
 						{feature.title}
 					</h3>
 					<p class="mt-1 text-gray-600 dark:text-neutral-400">
@@ -27,7 +31,7 @@
 		{/each}
 	</div>
 
-	<div style="position: relative;touch-action: none;">
+	<div style="position: relative; touch-action: none; **width: 100%;**">
 		<CanvasContext>
 			<Toolbox />
 			<CanvasBoard />
